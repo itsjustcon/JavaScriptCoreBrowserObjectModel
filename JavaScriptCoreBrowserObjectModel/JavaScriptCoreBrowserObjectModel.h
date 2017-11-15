1271751a3c034e311f7ab0be54962c8f814eb79c
Connor Grady <conair360@gmail.com>
2017-11-14 22:59:28 -0500

    Initial Commit

---
+++ JavaScriptCoreBrowserObjectModel/JavaScriptCoreBrowserObjectModel.h
@@ -0,0 +1,19 @@
+//
+//  JavaScriptCoreBrowserObjectModel.h
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 11/14/17.
+//  Copyright © 2017 Connor Grady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JavaScriptCoreBrowserObjectModel.
+FOUNDATION_EXPORT double JavaScriptCoreBrowserObjectModelVersionNumber;
+
+//! Project version string for JavaScriptCoreBrowserObjectModel.
+FOUNDATION_EXPORT const unsigned char JavaScriptCoreBrowserObjectModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JavaScriptCoreBrowserObjectModel/PublicHeader.h>
+
+
