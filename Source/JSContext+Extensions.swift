d1467e6a7940100f3c168dd96c55030a3dc0d33e
Connor Grady <conair360@gmail.com>
2017-12-12 23:53:15 -0500

    created `JSContext` & `JSValue` extensions

---
+++ Source/JSContext+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  JSContext+Extensions.swift
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 11/16/17.
+//  Copyright © 2017 Connor Grady. All rights reserved.
+//
+
+import JavaScriptCore
+import Foundation
+
+extension JSContext {
+    
+    open func setObject(_ object: Any!, forKeyedSubscript key: String!) {
+        return self.setObject(object, forKeyedSubscript: key as (NSCopying & NSObjectProtocol))
+    }
+    
+}
---
+++ Source/JSValue+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  JSValue+Extensions.swift
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 11/16/17.
+//  Copyright © 2017 Connor Grady. All rights reserved.
+//
+
+import JavaScriptCore
+import Foundation
+
+extension JSValue {
+    
+    open func setObject(_ object: Any!, forKeyedSubscript key: String!) {
+        return self.setObject(object, forKeyedSubscript: key as (NSCopying & NSObjectProtocol))
+    }
+    
+}
