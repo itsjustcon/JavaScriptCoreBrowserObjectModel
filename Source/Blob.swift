a4f07b4995ec85e2866d52898ce53a6430dc1a9a
Connor Grady <conair360@gmail.com>
2018-01-17 23:00:20 -0500

    fixed some methods in `Blob`

--- Source/Blob.swift
+++ Source/Blob.swift
@@ -13,8 +13,11 @@ import JavaScriptCore
     var size: Int { get }
     var type: String { get }
     //
-    init(_ blobParts: [JSValue], _ options: [String: String]?)
-    func slice(_ start: Int?, _ end: Int?, _ contentType: String?) -> Blob
+    //init(_ blobParts: [JSValue], _ options: [String: String]?)
+    init(_ blobParts: [JSValue], _ options: [String: String])
+    //func slice(_ start: Int?, _ end: Int?, _ contentType: String?) -> Blob
+    func slice(_ start: Int, _ end: Int, _ contentType: String) -> Blob
+    //func slice(_ start: JSValue?, _ end: JSValue?, _ contentType: String?) -> Blob
     // Events
     // - loadstart
     // - progress
@@ -29,12 +32,15 @@ import JavaScriptCore
     public var size: Int = 0
     public var type: String = ""
     
-    public required init(_ blobParts: [JSValue], _ options: [String : String]? = nil) {
+    //public required init(_ blobParts: [JSValue], _ options: [String: String]? = nil) {
+    public required init(_ blobParts: [JSValue], _ options: [String: String] = [ "type": "", "endings": "transparent" ]) {
         super.init()
     }
     
-    public func slice(_ start: Int? = 0, _ end: Int?, _ contentType: String? = "") -> Self {
-        <#code#>
+    //public func slice(_ start: Int? = 0, _ end: Int?, _ contentType: String? = "") -> Blob {
+    public func slice(_ start: Int = 0, _ end: Int, _ contentType: String = "") -> Blob {
+        print("Blob slice( start: \(String(describing: start)), end: \(String(describing: end)), contentType: \(String(describing: contentType)) )")
+        return Blob([])
     }
     
     
