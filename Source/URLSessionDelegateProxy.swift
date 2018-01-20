c6a87b6e56e6426814b214b10c3a264a899d3198
Connor Grady <conair360@gmail.com>
2018-01-20 02:46:27 -0500

    WIP: created `URLSessionDelegateProxy`

---
+++ Source/URLSessionDelegateProxy.swift
@@ -0,0 +1,9 @@
+//
+//  URLSessionDelegateProxy.swift
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 1/20/18.
+//  Copyright © 2018 Connor Grady. All rights reserved.
+//
+
+import Foundation
