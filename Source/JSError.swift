69701f1509a06c9a624f2577b6d546327ffcc6c6
Connor Grady <conair360@gmail.com>
2018-01-24 20:19:44 -0500

    WIP: created `JSError` & `JSPromise`

---
+++ Source/JSError.swift
@@ -0,0 +1,9 @@
+//
+//  JSError.swift
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 1/22/18.
+//  Copyright © 2018 Connor Grady. All rights reserved.
+//
+
+import Foundation
