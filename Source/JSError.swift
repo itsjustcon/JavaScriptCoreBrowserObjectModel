60d5114e3df5a80dfc8fec90474202ed3dfeaf84
Connor Grady <conair360@gmail.com>
2018-01-24 20:20:27 -0500

    WIP: basic `JSError` implementation

--- Source/JSError.swift
+++ Source/JSError.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import JavaScriptCore
+
+// SPEC: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+@objc public protocol ErrorJSProtocol: JSExport {
+    var name: String? { get set }
+    var message: String? { get set }
+    var stack: [JSValue] { get }
+    var fileName: String? { get }
+    var lineNumber: Int { get }
+    var columnNumber: Int { get }
+    //
+    //init(_ message: String?, _ fileName: String?, lineNumber: Int?)
+    init(_ message: String?, _ fileName: String?, lineNumber: JSValue?)
+    func toSource() -> String
+    func toString() -> String
+}
+
+@objc public class JSError: NSObject, ErrorJSProtocol {
+    
+    public var name: String?
+    public var message: String?
+    
+    public var stack: [JSValue] = [JSValue]()
+    
+    public var fileName: String?
+    public var lineNumber: Int = 0
+    public var columnNumber: Int = 0
+    
+    public required init(_ message: String?, _ fileName: String?, lineNumber: JSValue?) {
+        self.message = message
+        self.fileName = fileName
+        if let lineNumber = lineNumber, lineNumber.isNumber {
+            self.lineNumber = Int(lineNumber.toUInt32())
+        }
+    }
+    
+    public func toSource() -> String {
+        return ""
+    }
+    
+    public func toString() -> String {
+        return ""
+    }
+    
+    
+}
