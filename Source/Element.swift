8dbf38311bb23e3b4f8de2ff9f811bbf3e48d128
Connor Grady <conair360@gmail.com>
2019-05-11 09:38:05 -0400

    created `Element`

---
+++ Source/Element.swift
@@ -0,0 +1,9 @@
+//
+//  Element.swift
+//  JavaScriptCoreBrowserObjectModel
+//
+//  Created by Connor Grady on 5/6/19.
+//  Copyright © 2019 Connor Grady. All rights reserved.
+//
+
+import Foundation
